3a5d29d50e75f6fc9991b794629632039bc02622
Sascha <sascha.froelich@gmail.com>
2023-11-10 15:53:56 +0100

    0.38: Added file preproc.py for data preprocessing.

---
+++ preproc.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 10 14:00:09 2023
+
+@author: sascha
+"""
+
+import pickle
+import numpy as np
+import utils
+
+exp_behav_dict, expdata_df = utils.get_groupdata('/home/sascha/Desktop/vbm_torch/behav_data/')
+
+pickle.dump((exp_behav_dict, expdata_df), open("behav_fit/preproc_data.p", "wb" ) )
