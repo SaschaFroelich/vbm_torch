126: Added subject-specific WAIC computation and (hopefully) removed a bug in original WAIC computation.

inferencemodels.py
@@ -664,6 +664,11 @@ class GeneralGroupInference():
                 
         loglike = torch.zeros(num_samples, num_obs)
         like = torch.zeros(num_samples, num_obs)
+        
+        subject_WAIC = torch.zeros(self.num_agents)
+        subject_WAIC_var = torch.zeros(self.num_agents)
+        subject_loglike = {f'ag_{i}':[[] for _ in range(num_samples)] for i in range(self.num_agents)}
+        subject_like = {f'ag_{i}':[[] for _ in range(num_samples)] for i in range(self.num_agents)}
         for i in range(num_samples):
             print(f"Iterating to compute WAIC, step {i}.")
             conditioned_model = pyro.condition(self.model, 
@@ -678,16 +683,7 @@ class GeneralGroupInference():
                     obsmask = val['mask'].type(torch.int)
                     probs = val['fn'].probs
                     
-                    # if '799' in key:
-                    #     print("key:")
-                    #     print(key)
-                    #     print("probs:")
-                    #     print(probs)
-                    
                     choice_probs = probs[0, range(self.num_agents), choices]
-                    
-                    # if torch.any(choice_probs[0, torch.where(obsmask==1)[1]] == 0):
-                    #     ipdb.set_trace()
                     
                     '''
                         likelihood function
@@ -695,17 +691,29 @@ class GeneralGroupInference():
                     like[i, obsidx] += choice_probs[0, torch.where(obsmask==1)[1]].prod().detach()
                     loglike[i, obsidx] += torch.log(choice_probs[0, torch.where(obsmask==1)[1]]).sum().detach()
                     
-                    # if torch.isinf(loglike[i, obsidx]):
-                    #     ipdb.set_trace()
+                    for ag_idx in torch.where(obsmask==1)[1]:
+                        subject_like[f'ag_{ag_idx}'][i].append(choice_probs[0, ag_idx].item())
+                        subject_loglike[f'ag_{ag_idx}'][i].append(torch.log(choice_probs[0, ag_idx]).item())
                     
                     obsidx += 1
-                    
-            # ipdb.set_trace()
-            
+
         "effective number of parameters."
-        # pwaic = 2*((torch.log(like.mean(axis=0)/num_samples) - loglike.mean(axis=0)/num_samples).sum())
-        pwaic_vec = (2*torch.log(like.mean(axis=0)/num_samples) - 2*loglike.mean(axis=0)/num_samples)
-        pwaic = (2*torch.log(like.mean(axis=0)/num_samples) - 2*loglike.mean(axis=0)/num_samples).sum()
+        # pwaic_vec = 2*(torch.log(like.mean(axis=0)/num_samples) - loglike.mean(axis=0)/num_samples)
+        pwaic_vec = 2*(torch.log(like.mean(axis=0)) - loglike.mean(axis=0))
+        pwaic = pwaic_vec.sum()
+        
+        subject_pwaic = torch.zeros(self.num_agents)
+        subject_lppd = torch.zeros(self.num_agents)
+        for ag_idx in range(self.num_agents):
+            s_pwaic_vec = 2*(torch.log(torch.tensor(subject_like[f'ag_{ag_idx}']).mean(axis=0)) -
+                torch.tensor(subject_loglike[f'ag_{ag_idx}']).mean(axis=0))
+            s_pwaic = s_pwaic_vec.sum()
+            subject_pwaic[ag_idx] = s_pwaic
+            
+            s_lppd_vec = torch.log(torch.tensor(subject_like[f'ag_{ag_idx}'])).mean(axis=0)
+            s_lppd = s_lppd_vec.sum()
+            subject_lppd[ag_idx] = s_lppd
+            subject_WAIC[ag_idx] = s_lppd - s_pwaic
         
         lppd_vec = torch.log(like.mean(axis=0))
         lppd = lppd_vec.sum()
@@ -723,10 +731,9 @@ class GeneralGroupInference():
         assert cov.shape[0] == 2
         assert cov.shape[1] == 2
         waic_var = lppd_var + pwaic_var - 2*cov[0,1]
-        
+            
         print("Returning WAIC etc.")
-        # ipdb.set_trace()
-        return BIC.detach(), AIC.detach(), WAIC.detach(), loglike.mean(axis=0).sum().detach(), waic_var
+        return BIC.detach(), AIC.detach(), WAIC.detach(), loglike.mean(axis=0).sum().detach(), waic_var, subject_WAIC
     
     
 class GeneralGroupInferenceSTT():

parameter_recovery.py
@@ -68,22 +68,27 @@ Modelle:
 
 waithrs = 0
 post_pred = 0
-sim_model_day1 = 'Repbias_Conflict_Repdiff_lr'
-sim_models_day2 = ['Repbias_Conflict_Repdiff_lr']
+sim_model_day1 = 'Repbias_lr'
+sim_models_day2 = ['Repbias_lr']
 
 if 0:
     inf_model_day1 = sim_model_day1
     inf_models_day2 = sim_models_day2
     
 else:
-    inf_model_day1 = 'Repbias_Conflict_Repdiff_lr'
-    inf_models_day2 = ['Bullshitmodel3']
+    inf_model_day1 = 'Bullshitmodel'
+    inf_models_day2 = ['Bullshitmodel']
 
-num_inf_steps = 3_000
-halting_rtol = 1e-07 # for MLE estimation
 num_agents = 60
-posterior_pred_samples = 10
-num_waic_samples = 3_000
+num_inf_steps_day1 = 3_000
+halting_rtol_day1 = 1e-07 # for MLE estimation
+posterior_pred_samples_day1 = 10
+num_waic_samples_day1 = 3_000
+
+num_inf_steps_day2 = 1
+halting_rtol_day2 = 1e-02 # for MLE estimation
+posterior_pred_samples_day2 = 1
+num_waic_samples_day2 = 1
 
 #%%
 '''
@@ -140,14 +145,14 @@ Q_init_day1 = agent.Q_init
 print("===== Starting inference of day 1 =====")
 "----- Start Inference"
 infer = inferencemodels.GeneralGroupInference(agent, groupdata_dict_day1)
-agent_elbo_tuple = infer.infer_posterior(iter_steps = num_inf_steps, num_particles = 10)
+agent_elbo_tuple = infer.infer_posterior(iter_steps = num_inf_steps_day1, num_particles = 10)
 
 "----- Sample parameter estimates from posterior and add information to DataFrame"
 if post_pred:
-    firstlevel_df, secondlevel_df, predictive_choices, obs_mask = infer.posterior_predictives(n_samples = posterior_pred_samples)
+    firstlevel_df, secondlevel_df, predictive_choices, obs_mask = infer.posterior_predictives(n_samples = posterior_pred_samples_day1)
     
 else:
-    firstlevel_df = infer.sample_posterior(n_samples = posterior_pred_samples)
+    firstlevel_df = infer.sample_posterior(n_samples = posterior_pred_samples_day1)
     secondlevel_df = None
     predictive_choices = None
     obs_mask = None
@@ -159,8 +164,8 @@ firstlevel_df['inf_model'] = [inf_model_day1]*len(firstlevel_df)
 ID_df = group_behav_df_day1.loc[:, ['ag_idx']].drop_duplicates()
 
 "----- MLE & IC"
-max_log_like, mle_locs = infer.train_mle(halting_rtol = halting_rtol)
-_, _, WAIC, ll, WAIC_var = infer.compute_IC(num_samples = num_waic_samples)
+max_log_like, mle_locs = infer.train_mle(halting_rtol = halting_rtol_day1)
+_, _, WAIC, ll, WAIC_var, subject_WAIC = infer.compute_IC(num_samples = num_waic_samples_day1)
 
 "----- Q_init & seqcounter for next day"
 seq_counter_day2 = infer.agent.seq_counter.detach()
@@ -199,12 +204,13 @@ extra_storage = (Q_init_day1,
                  secondlevel_df,
                  param_names_day1,
                  'recovery',
-                 halting_rtol,
+                 halting_rtol_day1,
                  WAIC,
                  ll,
                  predictive_choices,
                  obs_mask,
-                 WAIC_var)
+                 WAIC_var,
+                 subject_WAIC)
 
 filename_day1 = f'recovery_infmodel_{inf_model_day1}_simmodel_{sim_model_day1}_day{day}_{timestamp}_{num_agents}agents'
 pickle.dump( (firstlevel_df, 
@@ -248,14 +254,14 @@ for md2_idx in range(len(sim_models_day2)):
     print("===== Starting inference of day 2 =====")
     "----- Start Inference"
     infer = inferencemodels.GeneralGroupInference(agent, groupdata_dict_day2)
-    agent_elbo_tuple = infer.infer_posterior(iter_steps = num_inf_steps, num_particles = 10)
+    agent_elbo_tuple = infer.infer_posterior(iter_steps = num_inf_steps_day2, num_particles = 10)
     
     "----- Sample parameter estimates from posterior and add information to DataFrame"
     if post_pred:
-        firstlevel_df, secondlevel_df, predictive_choices, obs_mask = infer.posterior_predictives(n_samples = posterior_pred_samples)
+        firstlevel_df, secondlevel_df, predictive_choices, obs_mask = infer.posterior_predictives(n_samples = posterior_pred_samples_day2)
         
     else:
-        firstlevel_df = infer.sample_posterior(n_samples = posterior_pred_samples)
+        firstlevel_df = infer.sample_posterior(n_samples = posterior_pred_samples_day2)
         secondlevel_df = None
         
     firstlevel_df['group'] = firstlevel_df['ag_idx'].map(lambda x: groupdata_dict_day2['group'][0][x])
@@ -266,8 +272,8 @@ for md2_idx in range(len(sim_models_day2)):
     # firstlevel_df['ID'] = firstlevel_df['ag_idx'].map(lambda x: groupdata_dict_day2['ID'][0][x])
     
     "----- MLE & IC"
-    max_log_like, mle_locs = infer.train_mle(halting_rtol = halting_rtol)
-    BIC, AIC, WAIC, ll, WAIC_var = infer.compute_IC(num_samples = num_waic_samples)
+    max_log_like, mle_locs = infer.train_mle(halting_rtol = halting_rtol_day2)
+    BIC, AIC, WAIC, ll, WAIC_var, subject_WAIC = infer.compute_IC(num_samples = num_waic_samples_day2)
     
     # "----- Q_init for next day"
     # Q_init_day2 = agent.Q[-1].detach().mean(axis=0)[None, ...]
@@ -286,12 +292,13 @@ for md2_idx in range(len(sim_models_day2)):
                      secondlevel_df, # 9)
                      param_names_day2, # 10)
                      'recovery', # 11)
-                     halting_rtol, # 12)
+                     halting_rtol_day2, # 12)
                      WAIC, # 13)
                      ll, # 14)
                      predictive_choices, # 15)
                      obs_mask, # 16)
-                     WAIC_var)
+                     WAIC_var,
+                     subject_WAIC)
     
     pickle.dump( (firstlevel_df, 
                   group_behav_df_day2, 